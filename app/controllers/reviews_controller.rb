8e0d4191e875295c253c880adcf7951e6220469f
Nicko <nickodc96@gmail.com>
2018-04-20 11:39:43 -0700

    add create and edit func

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,16 @@ class ReviewsController < ApplicationController
   @food = Food.find(params[:food_id])
   @review = @food.reviews.new
   end
+
+
+  def create
+    @food = Food.find(params[:food_id])
+    @review = @food.reviews.new(reviews_params)
+    if @food.save
+      redirect_to food_path(@reviews.list)
+    else
+      render :new
+    end
+  end
+
 end
