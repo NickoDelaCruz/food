277a46c8c5fdcabfff01e9a3482c6229b0f2f1a4
Nicko <nickodc96@gmail.com>
2018-04-22 19:52:25 -0700

    add lowcost in product controller, add sort date/cost scope in product.model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true,  numericality: true
   validates :country, :presence => true
+
+  scope :sort_date, -> {order(created_at: :desc).limit(3)}
+  scope :sort_cost, -> {order(cost: :asc).limit(5)}
+
+
+
 end
