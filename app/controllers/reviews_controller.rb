91feee81fd1e1e239d46aac54096ee1c46015467
Nicko <nickodc96@gmail.com>
2018-04-22 20:32:40 -0700

    add functions review_controller and flash notice

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,54 @@
 class ReviewsController < ApplicationController
   def index
-  @reviews = Review.all
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    @product = @review.product
+
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review Successfully added"
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
   end
 end
