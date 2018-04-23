d3f2ff9cd8f7a2a3e335775e82d6e0d9e14dcecf
Nicko <nickodc96@gmail.com>
2018-04-22 20:41:13 -0700

    refactoring faker seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,2 @@
-5.times do |index|
-  Food.create!(name: Faker::Book.title,
-                image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
-                description: Faker::Lorem.sentence(20, false, 0).chop,
-                area: Faker::Lorem.sentence(20, false, 0).chop
-end
+Product.destroy_all
+Review.destroy_all
