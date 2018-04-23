ca297ab5b39e20d6242fed8366c0ea24067bf0de
Nicko <nickodc96@gmail.com>
2018-04-22 20:46:17 -0700

    add loop to make 50 products with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 Product.destroy_all
 Review.destroy_all
+
+50.times do |index|
+  product = Product.create!(name: Faker::Hipster.word,
+  image: Faker::LoremPixel.image("200x200", false, 'food'),
+  country: Faker::Address.country,
+  cost: Faker::Number.decimal(2,2)
+  )
+end
